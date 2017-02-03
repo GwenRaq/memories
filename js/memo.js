2470f880479460246c20db6b3ff9d4e8066bb31c
GwenRaq <gwen.raq@gmail.com>
2017-02-03 08:25:59 -1000

    flips back

--- js/memo.js
+++ js/memo.js
@@ -3,4 +3,8 @@ function flip(clicked_id) {
 	var j = document.getElementById(clicked_id);
     j.style.transform = 'rotateY(180deg)';
     j.style.transitionDuration = "0.5s"
+    if(j.style.transform === 'rotateY(180deg)'){
+    	setTimeout(function(){j.style.transform = 'rotate(0deg)';
+    },1000);
   }
+}
