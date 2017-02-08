80bc29cf642aa6471a13a03058afff350965e1d5
GwenRaq <gwen.raq@gmail.com>
2017-02-08 09:21:22 -1000

    hhhhh

--- js/memo.js
+++ js/memo.js
@@ -9,15 +9,17 @@ function flip(clicked_id) {
   }
 }
 
-// var images = ["a","b", "c", "d","e","f","g","h"];
 
-// get images, place them in an array & randomize the order
-// for (var i = 0; i < 8; i++) { 
-//   var rand = Math.floor(Math.random() * (pic.length));
-//   var img = '../memories/assets/card' + rand + '.png';
-//   document.getElementsByClassName("front")[i].style.backgroundImage = "url('./memories/assets/card"+ rand +".png')";
-//   images.push(img);
-//   images.push(img);
-// }
-// randomizeImages();
+var images = ["url('../memories/assets/card/yess.png')", "url('../memories/assets/card/pandaburd.png')","url('../memories/assets/card/pandaburd.png')","url('../memories/assets/large.gif')",];
 
+function randis(){
+	var pic=[1,1,2,2,3,3,4,4];
+	for (var i = 0; i < 8; i++) {
+		document.getElementById(images[i]).style.display="inline";
+		var randomNumber =  Math.floor(Math.random() * (images.length));
+		//document.getElementsByClassName("front")[i].style.backgroundImage = "url('../memories/assets/card/"+pic[randomNumber]+".png')";
+		//pair.push([images[i],pic[randomNumber]]);
+		pic.splice(randomNumber, 1);
+	}
+}
+randis();
