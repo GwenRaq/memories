3b088a8765947c342abc8968a897c0a821b7143c
GwenRaq <gwen.raq@gmail.com>
2017-02-10 22:48:40 -1000

    Update memo.js

--- js/memo.js
+++ js/memo.js
@@ -1,9 +1,9 @@
 // var ids = ["ichi","nii","san","shi","go","roku","nana","hachi"];
 // var pics = [1,1,2,2,3,3,4,4];
 // var i;
-var rotationQueue = [];
-var picQueue = [];
-var okay = 0; 
+// var rotationQueue = [];
+// var picQueue = [];
+// var okay = 0; 
 
 // function randomizer() {
 // 	console.log("k");
@@ -21,32 +21,32 @@ var okay = 0;
 // 	}
 // randomizer();
 
- function flipDis(clicked_id) {
-  document.getElementById(clicked_id).style.transform = 'rotateY(180deg)';
-  var idMatch = showBack.getElementsByClassName("back")[0].id;
-  	if(rotationQueue.length < 2){
-    	rotationQueue.push(clicked_id); 
-    	picQueue.push(showBack.getElementsByClassName("back")[0].id;); 
-  } 
-console.log(okay);
-  if (rotationQueue.length == 2) {
-    setTimeout(function(){
-      document.getElementById(rotationQueue[0]).style.transform = "rotateY(0deg)";
-      document.getElementById(rotationQueue[1]).style.transform = "rotateY(0deg)";
-    }, 1000);
-    setTimeout(function() {
-      rotationQueue.splice(0, 2);
-      picQueue.splice(0, 2);
-    }, 1050);
-	}
-}
-
 //  function flipDis(clicked_id) {
-//   document.getElementById(clicked_id).style.transform= 'rotateY(180deg)';
-
-//   if(document.getElementById(clicked_id).style.transform === 'rotateY(180deg)'){
+//   document.getElementById(clicked_id).style.transform = 'rotateY(180deg)';
+//   var idMatch = showBack.getElementsByClassName("back")[0].id;
+//   	if(rotationQueue.length < 2){
+//     	rotationQueue.push(clicked_id); 
+//     	picQueue.push(idMatch); 
+//   } 
+// console.log(okay);
+//   if (rotationQueue.length == 2) {
 //     setTimeout(function(){
-//     document.getElementById(clicked_id).style.transform = 'rotateY(0deg)';
-//     },1000);
-//   }
+//       document.getElementById(rotationQueue[0]).style.transform = "rotateY(0deg)";
+//       document.getElementById(rotationQueue[1]).style.transform = "rotateY(0deg)";
+//     }, 1000);
+//     setTimeout(function() {
+//       rotationQueue.splice(0, 2);
+//       picQueue.splice(0, 2);
+//     }, 1050);
+// 	}
 // }
+
+ function flipDis(clicked_id) {
+  document.getElementById(clicked_id).style.transform= 'rotateY(180deg)';
+
+  if(document.getElementById(clicked_id).style.transform === 'rotateY(180deg)'){
+    setTimeout(function(){
+    document.getElementById(clicked_id).style.transform = 'rotateY(0deg)';
+    },1000);
+  }
+}
