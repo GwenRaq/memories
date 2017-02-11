35d7365ab71c2b460fa7b729e191bc2b4f5f312e
GwenRaq <gwen.raq@gmail.com>
2017-02-10 20:17:59 -1000

    Update memo.js

--- js/memo.js
+++ js/memo.js
@@ -1,55 +1,43 @@
+var ids = ["a","b","c","d","e","f","g","h"];
+var cardPics = [1,1,2,2,3,3,4,4];
+
+function randomizer() {
+  for (i = 0; i < 8; i++){  
+		var randomNum = Math.floor(Math.random()*(pics.length));
+		var setImg = document.getElementsByClassName("back")[i];
+		if (pics[randomNum] === 2 || pics[randomNum] === 3) {
+			setImg.style.backgroundImage = "url('../memories/assets/card"+pics[randomNum]+".png')";
+      		setImg.id = ""+pics[randomNum]+"";
+		} else {
+			setImg.style.backgroundImage = "url('../memories/assets/card"+pics[randomNum]+".jpeg')";
+      		setImg.id = ""+pics[randomNum]+"";
+		}
+		cardPics.splice(randomNum, 1);
+	}
 
 function flip(clicked_id) {
 	var j = document.getElementById(clicked_id);
-    j.style.transform = 'rotateY(180deg)';
-    j.style.transitionDuration = "0.5s"
-    if(j.style.transform === 'rotateY(180deg)'){
-    	setTimeout(function(){j.style.transform = 'rotate(0deg)';
-    },1000);
-  }
+ 	j.style.transform = 'rotateY(180deg)';
+	var idMatch = showBack.getElementsByClassName("back")[0].id;
+  	if(rotationQueue.length < 2){
+    	rotationQueue.push(clicked_id); 
+    	picQueue.push(idMatch); 
+//look up how to access id within div
+  } 
+console.log(x);
 
 
-// var images = [];
-// images[0] = '../memories/assets/card/yess.png';
-// images[1] = '../memories/assets/card/yess.png';
-// images[2] = '../memories/assets/card/pandaburd.png';
-// images[3] = '../memories/assets/large.gif';
-// images[4] = '../memories/assets/card/carot.png';
-// images[5] = '../memories/assets/card/pandaburd.png';
-// images[6] = '../memories/assets/card/carot.png';
-// images[7] ='../memories/assets/large.gif';
 
-// var clicks = 0;
-// var firstSelect;
-// var secondSelect;
+// function flip(idName) {
+//   var showBack = document.getElementById(idName);
+//   showBack.style.transform = "rotateY(180deg)";
+ 
 
-// function select(card){
-//   if(clicks === 2 && j.style.transform === 'rotateY(180deg)'){
-//     return;
-//   }
-//   if(clicks === 0){
-//     firstSelect = card;
-//     document.images[card].src = cardImage[card];
-//     clicks = 1;
-//   } else {
-//     clicks = 2;
-//     secondSelect = card;
-//     document.images[card].src = cardImage[card];
-//     timer = setInterval("check()", 1000);
-//   }
-//   console.log('poop', clicks);
-//   console.log('selection', firstSelect);
-//   console.log('selection2', secondSelect);
-// }
-
-
-// 	var pic=[1,1,2,2,3,3,4,4];
-// 	for (var i = 0; i < 8; i++) {
-// 		document.getElementById(images[i]).style.display="inline";
-// 		var randomNumber =  Math.floor(Math.random() * (images.length));
-// 		//document.getElementsByClassName("front")[i].style.backgroundImage = "url('../memories/assets/card/"+pic[randomNumber]+".png')";
-// 		//pair.push([images[i],pic[randomNumber]]);
-// 		pic.splice(randomNumber, 1);
-// 	}
-// }
-// randis();
+//   //checks if two cards are flipped   
+//   var idMatch = showBack.getElementsByClassName("back")[0].id;
+//   if(rotationQueue.length < 2){
+//     rotationQueue.push(idName); 
+//     picQueue.push(idMatch); 
+//     //^ after seeing that pic ids are sam   e, deletes divs, look up how to access id within div
+//   } 
+//   console.log(x);
